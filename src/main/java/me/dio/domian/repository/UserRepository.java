c7c307b1b94715a616b49c23e964f3551a2ab443
Thiago Ferrer <thi-ferrer@live.com>
2025-09-19 16:05:45 -0300

    Implementação da camada de dominio

---
+++ src/main/java/me/dio/domian/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.dio.domian.repository;
+
+import me.dio.domian.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long>{
+
+}
